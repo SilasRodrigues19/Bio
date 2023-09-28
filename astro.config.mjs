fbca691db6ef15a1329fcde5fd942306edbb3e9d
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-28 19:13:09 -0300

    fix: remove astro-compress to fix svg color issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bio-silas.vercel.app/',
-  integrations: [tailwind(), sitemap(), compress()]
+  integrations: [tailwind(), sitemap()]
 });
