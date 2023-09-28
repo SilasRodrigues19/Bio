3da9ec2a3aaf7e58a693fbe57e5c9f15f3c4bd7a
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-28 16:14:26 -0300

    feat: add initial files

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			current: 'currentColor',
+			transparent: 'transparent',
+			primary: colors.slate,
+		},
+		fontFamily: {
+			sans: ['Inter', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
