c691d83fc51ea72b7cf9ad005c7af5fe9d17690e
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-28 19:01:07 -0300

    chore: add sitemap and astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://bio-silas.vercel.app/',
+  integrations: [tailwind(), sitemap(), compress()]
 });
