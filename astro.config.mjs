12196e79f42034f34d00b996716a7333db875270
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-02 16:29:01 -0300

    chore: update link preview to fix http headers issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import linkPreview from 'astro-link-preview';
+
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bio-silas.vercel.app/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), linkPreview()],
 });
