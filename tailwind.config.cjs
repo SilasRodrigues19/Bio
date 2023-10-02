2f1599a530ce6917c70b05d0dafa9ca9185f26fc
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-02 16:27:22 -0300

    refactor: adjust tailwind font sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   darkMode: 'class',
@@ -14,7 +15,24 @@ module.exports = {
     fontFamily: {
       sans: ["'Saira'", 'sans-serif'],
     },
-    extend: {},
+    fontSize: {
+      xs: ['0.75rem', '1rem'],
+      sm: ['0.875rem', '1.25rem'],
+      base: ['1rem', '1.75rem'],
+      lg: ['1.125rem', '2rem'],
+      xl: ['1.25rem', '2.125rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['1.875rem', '2.375rem'],
+      '4xl': ['2.25rem', '2.75rem'],
+      '5xl': ['3rem', '3.5rem'],
+      '6xl': ['3.75rem', '4.25rem'],
+    },
+    extend: {
+      screens: {
+        xs: '300px',
+        ...defaultTheme.screens,
+      },
+    },
   },
   plugins: [],
 };
