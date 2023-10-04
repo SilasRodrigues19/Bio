0b5d26e9950bd4a4b3399d26a8e67b5fa075d8fa
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-04 13:36:05 -0300

    chore: add i18n utils

---
+++ src/i18n/ui.ts
@@ -0,0 +1,30 @@
+export const languages = {
+  en: 'English',
+  pt: 'Portuguese',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    '404.title': 'Not found | Bio - Silas Rodrigues',
+    '404.description':
+      'Page not found. Please, check the URL in the address bar and try again',
+    '404.status': 'Page not found',
+    '404.message': 'Please, check the URL in the address bar and try again.',
+    '404.button': 'Go back to home',
+
+  },
+  pt: {
+    '404.title': 'Não encontrado | Bio - Silas Rodrigues',
+    '404.description':
+      'Página não encontrada. Por favor, verifique a URL na barra de endereços e tente novamente',
+    '404.status': 'Página não encontrada',
+    '404.message':
+      'Por favor, verifique a URL na barra de endereços e tente novamente.',
+    '404.button': 'Voltar para o início',
+
+  },
+} as const;
+
+export const showDefaultLang = false;
