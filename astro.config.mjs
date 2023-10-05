b777e86710e98c328c02217b9f3171a8607a2c2c
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-05 13:11:34 -0300

    chore: add astro-compress with svg ignore rule

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import linkPreview from 'astro-link-preview';
 
-
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bio-silas.vercel.app/',
-  integrations: [tailwind(), sitemap(), linkPreview()],
+  integrations: [tailwind(), sitemap(), linkPreview(), 
+    compress({
+      SVG: false,
+      Exclude: [
+        (File) => File.match(/\/assets\/.*\.svg$/),
+      ]
+  })]
 });
