3c8fa6bb1579ba2eb7ab14f01c0e088834a2ae16
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-14 15:04:53 -0300

    refactor: add translation to header and footer words

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,18 @@ export const ui = {
     '404.message': 'Please, check the URL in the address bar and try again.',
     '404.button': 'Go back to home',
 
+    'header.about': 'About',
+    'header.social': 'Social',
+    'header.contact': 'Contact',
+
+    'footer.description': 'Bio - Silas Rodrigues. About my career and projects, social networks and contact information.',
+    'footer.nav': 'Navigation',
+    'footer.nav.home': 'Home',
+    'footer.nav.about': 'About',
+    'footer.nav.social': 'Social',
+    'footer.nav.contact': 'Contact',
+    'footer.copy': 'All rights reserved.',
+    'footer.terms': 'Terms and Privacy',
   },
   pt: {
     '404.title': 'Não encontrado | Bio - Silas Rodrigues',
@@ -24,6 +36,18 @@ export const ui = {
       'Por favor, verifique a URL na barra de endereços e tente novamente.',
     '404.button': 'Voltar para o início',
 
+    'header.about': 'Sobre',
+    'header.social': 'Social',
+    'header.contact': 'Contato',
+
+    'footer.description': 'Bio - Silas Rodrigues. Sobre minha carreira e projetos, redes sociais e informações de contato.',
+    'footer.nav': 'Navegação',
+    'footer.nav.home': 'Início',
+    'footer.nav.about': 'Sobre',
+    'footer.nav.social': 'Social',
+    'footer.nav.contact': 'Contato',
+    'footer.copy': 'Todos os direitos reservados.',
+    'footer.terms': 'Termos e Privacidade',
   },
 } as const;
 
