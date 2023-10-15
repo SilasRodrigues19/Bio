c83c9c5c52d5f972664fc9e9732bd8fbbe5c673e
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-15 12:27:18 -0300

    feat: add page to terms and privacy

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,6 +14,24 @@ export const ui = {
     'breadcrumb.en': 'English',
     'breadcrumb.pt': 'Portuguese',
 
+    'privacy.layout.description':
+      'Our privacy policy will help you understand what information is collected and how it is used.',
+    'privacy.layout.title': 'Bio - Silas Rodrigues | Privacy Policy',
+    'privacy.title': 'Privacy Policy',
+    'privacy.update': 'Last updated: August 15, 2023',
+    'privacy.intro':
+      'We are committed to safeguarding your privacy. This Privacy Policy outlines how we collect, use, and protect your personal information when you use our website. Please take a moment to read through this document to understand our practices.',
+    'privacy.item1.title': '1. Information We Collect',
+    'privacy.item1.subtitle':
+      'We may collect the following types of personal information when you interact with our website:',
+    'privacy.item1.content': ' When accessing the contact page, we only collect the information you provide when submitting the form. We do not share this information with third parties.',
+    'privacy.item2.title': '2. How We Use Your Information',
+    'privacy.item2.subtitle':
+      'We may use your personal information for the following purposes:',
+    'privacy.item2.text': 'Communication: we only use your email address or phone number for contact within the context of the message received, without any type of spam.',
+    'privacy.contact.title': 'Contact Us',
+    'privacy.contact.text': 'If you have any questions or concerns about our Privacy Policy, please contact us at.',
+
     '404.title': 'Not found | Bio - Silas Rodrigues',
     '404.description':
       'Page not found. Please, check the URL in the address bar and try again',
@@ -42,6 +60,25 @@ export const ui = {
     'breadcrumb.contact': 'Contato',
     'breadcrumb.en': 'Inglês',
     'breadcrumb.pt': 'Português',
+
+    'privacy.layout.description':
+      'Nossa política de privacidade ajudará você a entender quais informações são coletadas e como elas são usadas.',
+    'privacy.layout.title': 'Bio - Silas Rodrigues | Política de Privacidade',
+    'privacy.title': 'Política de Privacidade',
+    'privacy.update': 'Última atualização: 15 de agosto de 2023',
+    'privacy.intro':
+      'Estamos comprometidos em proteger sua privacidade. Esta Política de Privacidade descreve como coletamos, usamos e protegemos suas informações pessoais quando você usa nosso site. Reserve um momento para ler este documento e entender nossas práticas.',
+    'privacy.item1.title': '1. Informações que coletamos',
+    'privacy.item1.subtitle':
+      'Podemos coletar os seguintes tipos de informações pessoais quando você interage com nosso site:',
+    'privacy.item1.content': 'Ao acessar a página de contato, coletamos apenas as informações que você fornece ao enviar o formulário. Não compartilhamos essas informações com terceiros.',
+    'privacy.item2.title': '2. Como usamos suas informações',
+    'privacy.item2.subtitle':
+      'Podemos usar suas informações pessoais para os seguintes fins:',
+    'privacy.item2.text': 'Comunicação: usamos apenas seu endereço de e-mail ou número de telefone para contato no contexto da mensagem recebida, sem nenhum tipo de spam.',
+    'privacy.contact.title': 'Entre em contato',
+    'privacy.contact.text': 'Se você tiver alguma dúvida ou preocupação com nossa Política de Privacidade, entre em contato conosco em.',
+    
 
     '404.title': 'Não encontrado | Bio - Silas Rodrigues',
     '404.description':
