0a8949f40240007cf2ada0299403e037801ede87
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-19 14:35:18 -0300

    style: format with prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,36 +12,34 @@ delete colors['blueGray'];
 colors = { ...colors, ...{ transparent: 'transparent' } };
 
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
       ...colors,
       primary: colors.slate,
     },
     fontFamily: {
-      sans: ["'Saira'", 'sans-serif'],
+      sans: ["'Saira'", "sans-serif"],
     },
     fontSize: {
-      xs: ['0.75rem', '1rem'],
-      sm: ['0.875rem', '1.25rem'],
-      base: ['1rem', '1.75rem'],
-      lg: ['1.125rem', '2rem'],
-      xl: ['1.25rem', '2.125rem'],
-      '2xl': ['1.5rem', '2rem'],
-      '3xl': ['1.875rem', '2.375rem'],
-      '4xl': ['2.25rem', '2.75rem'],
-      '5xl': ['3rem', '3.5rem'],
-      '6xl': ['3.75rem', '4.25rem'],
+      xs: ["0.75rem", "1rem"],
+      sm: ["0.875rem", "1.25rem"],
+      base: ["1rem", "1.75rem"],
+      lg: ["1.125rem", "2rem"],
+      xl: ["1.25rem", "2.125rem"],
+      "2xl": ["1.5rem", "2rem"],
+      "3xl": ["1.875rem", "2.375rem"],
+      "4xl": ["2.25rem", "2.75rem"],
+      "5xl": ["3rem", "3.5rem"],
+      "6xl": ["3.75rem", "4.25rem"],
     },
     extend: {
       screens: {
-        xs: '300px',
+        xs: "300px",
         ...defaultTheme.screens,
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [require("tailwind-scrollbar")],
 };
