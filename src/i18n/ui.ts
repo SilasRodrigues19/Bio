e9dd0d8bf066751a74ec63b05efbf8627d435a50
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-10-30 21:34:39 -0300

    refactor: add i18n and text color scale

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,120 +7,159 @@ export const defaultLang = 'en';
 
 export const ui = {
   en: {
-    'sr-only-language': 'Change language',
-    'sr-only-theme': 'Toggle theme',
-    'sr-only-menu': 'Toggle menu',
-    'sr-only-page': 'Current page',
-    'sr-only-footer': 'Footer',
-    'sr-only-header': 'Header',
+    "home.layout.title": "Bio - Silas Rodrigues | Home",
+    "home.layout.description":
+      "Silas Rodrigues: Front-end Developer skilled in Astro, React, TypeScript, Tailwind, Next.js, Node.js, MongoDB, and Prisma. Explore my profile for social media links, personal links, and contact information.",
+    "about.layout.title": "Bio - Silas Rodrigues | About",
+    "about.layout.description":
+      "Get to know Silas Rodrigues, a self-taught web developer from São Paulo, Brazil. Explore his journey into technology, his interests in web development, and his proficiency in JavaScript ecosystem. Discover a professional always eager for new challenges and growth opportunities.",
+    "contact.layout.title": "Bio - Silas Rodrigues | Contact",
+    "contact.layout.description":
+      "Connect with Silas Rodrigues, a passionate web developer based in São Paulo, Brazil. Reach out for collaborations, inquiries, or just to say hello. Get in touch and let's create something amazing together.",
+    "resume.layout.title": "Bio - Silas Rodrigues | Resume",
+    "resume.layout.description":
+      "Explore the professional journey and skills of Silas Rodrigues through his detailed resume. Get insights into his experience, expertise, and accomplishments in web development and technology.",
+    "social.layout.title": "Bio - Silas Rodrigues | Social",
+    "social.layout.description":
+      "Explore a collection of Silas Rodrigues' most relevant personal information, open-source projects and social media profiles. Connect with him through platforms like Portfolio, Github, Twitter, and more.",
 
-    'layout.scroll': 'Scroll to top',
+    "sr-only-language": "Change language",
+    "sr-only-theme": "Toggle theme",
+    "sr-only-menu": "Toggle menu",
+    "sr-only-page": "Current page",
+    "sr-only-footer": "Footer",
+    "sr-only-header": "Header",
 
-    'breadcrumb.home': 'Home',
-    'breadcrumb.social': 'Social',
-    'breadcrumb.about': 'About',
-    'breadcrumb.contact': 'Contact',
-    'breadcrumb.privacy': 'Privacy',
-    'breadcrumb.en': 'English',
-    'breadcrumb.pt': 'Portuguese',
+    "layout.scroll": "Scroll to top",
 
-    'privacy.layout.description':
-      'Our privacy policy will help you understand what information is collected and how it is used.',
-    'privacy.layout.title': 'Bio - Silas Rodrigues | Privacy Policy',
-    'privacy.title': 'Privacy Policy',
-    'privacy.update': 'Last updated: August 15, 2023',
-    'privacy.intro':
-      'We are committed to safeguarding your privacy. This Privacy Policy outlines how we collect, use, and protect your personal information when you use our website. Please take a moment to read through this document to understand our practices.',
-    'privacy.item1.title': '1. Information We Collect',
-    'privacy.item1.subtitle':
-      'We may collect the following types of personal information when you interact with our website:',
-    'privacy.item1.content': ' When accessing the contact page, we only collect the information you provide when submitting the form. We do not share this information with third parties.',
-    'privacy.item2.title': '2. How We Use Your Information',
-    'privacy.item2.subtitle':
-      'We may use your personal information for the following purposes:',
-    'privacy.item2.text': 'Communication: we only use your email address or phone number for contact within the context of the message received, without any type of spam.',
-    'privacy.contact.title': 'Contact Us',
-    'privacy.contact.text': 'If you have any questions or concerns about our Privacy Policy, please contact us at.',
+    "breadcrumb.home": "Home",
+    "breadcrumb.social": "Social",
+    "breadcrumb.about": "About",
+    "breadcrumb.contact": "Contact",
+    "breadcrumb.privacy": "Privacy",
+    "breadcrumb.resume": "Resume",
+    "breadcrumb.en": "English",
+    "breadcrumb.pt": "Portuguese",
 
-    '404.title': 'Not found | Bio - Silas Rodrigues',
-    '404.description':
-      'Page not found. Please, check the URL in the address bar and try again',
-    '404.status': 'Page not found',
-    '404.message': 'Please, check the URL in the address bar and try again.',
-    '404.button': 'Go back to home',
+    "privacy.layout.description":
+      "Our privacy policy will help you understand what information is collected and how it is used.",
+    "privacy.layout.title": "Bio - Silas Rodrigues | Privacy Policy",
+    "privacy.title": "Privacy Policy",
+    "privacy.update": "Last updated: August 15, 2023",
+    "privacy.intro":
+      "We are committed to safeguarding your privacy. This Privacy Policy outlines how we collect, use, and protect your personal information when you use our website. Please take a moment to read through this document to understand our practices.",
+    "privacy.item1.title": "1. Information We Collect",
+    "privacy.item1.subtitle":
+      "We may collect the following types of personal information when you interact with our website:",
+    "privacy.item1.content":
+      " When accessing the contact page, we only collect the information you provide when submitting the form. We do not share this information with third parties.",
+    "privacy.item2.title": "2. How We Use Your Information",
+    "privacy.item2.subtitle":
+      "We may use your personal information for the following purposes:",
+    "privacy.item2.text":
+      "Communication: we only use your email address or phone number for contact within the context of the message received, without any type of spam.",
+    "privacy.contact.title": "Contact Us",
+    "privacy.contact.text":
+      "If you have any questions or concerns about our Privacy Policy, please contact us at.",
 
-    'header.about': 'About',
-    'header.social': 'Social',
-    'header.contact': 'Contact',
+    "404.title": "Not found | Bio - Silas Rodrigues",
+    "404.description":
+      "Page not found. Please, check the URL in the address bar and try again",
+    "404.status": "Page not found",
+    "404.message": "Please, check the URL in the address bar and try again.",
+    "404.button": "Go back to home",
 
-    'footer.description':
-      'Bio - Silas Rodrigues. Career, projects and how to find me online.',
-    'footer.nav': 'Navigation',
-    'footer.nav.home': 'Home',
-    'footer.nav.about': 'About',
-    'footer.nav.social': 'Social',
-    'footer.nav.contact': 'Contact',
-    'footer.copy': 'All rights reserved.',
-    'footer.terms': 'Terms and Conditions',
+    "header.about": "About",
+    "header.social": "Social",
+    "header.contact": "Contact",
+
+    "footer.description":
+      "Bio - Silas Rodrigues. Career, projects and how to find me online.",
+    "footer.nav": "Navigation",
+    "footer.nav.home": "Home",
+    "footer.nav.about": "About",
+    "footer.nav.social": "Social",
+    "footer.nav.contact": "Contact",
+    "footer.copy": "All rights reserved.",
+    "footer.terms": "Terms and Conditions",
   },
   pt: {
-    'sr-only-language': 'Alterar idioma',
-    'sr-only-theme': 'Alternar tema',
-    'sr-only-menu': 'Alternar menu',
-    'sr-only-page': 'Página atual',
-    'sr-only-footer': 'Rodapé',
-    'sr-only-header': 'Cabeçalho',
+    "home.layout.title": "Bio - Silas Rodrigues | Início",
+    "home.layout.description":
+      "Silas Rodrigues: Desenvolvedor Front-end especializado em Astro, React, TypeScript, Tailwind, Next.js, Node.js, MongoDB e Prisma. Explore meu perfil para links de redes sociais, links pessoais e informações de contato.",
+    "about.layout.title": "Bio - Silas Rodrigues | Sobre",
+    "about.layout.description":
+      "Conheça Silas Rodrigues, um desenvolvedor web autodidata de São Paulo, Brasil. Explore sua jornada na tecnologia, seus interesses em desenvolvimento web e sua proficiência no ecossistema JavaScript. Descubra um profissional sempre ávido por novos desafios e oportunidades de crescimento.",
+    "contact.layout.title": "Bio - Silas Rodrigues | Contato",
+    "contact.layout.description":
+      "Conecte-se com Silas Rodrigues, um desenvolvedor web apaixonado baseado em São Paulo, Brasil. Entre em contato para colaborações, consultas ou apenas para dizer olá. Entre em contato e vamos criar algo incrível juntos.",
+    "resume.layout.title": "Bio - Silas Rodrigues | Currículo",
+    "resume.layout.description":
+      "Explore a jornada profissional e as habilidades de Silas Rodrigues por meio de seu currículo detalhado. Obtenha informações sobre sua experiência, especialização e realizações em desenvolvimento web e tecnologia.",
+    "social.layout.title": "Bio - Silas Rodrigues | Social",
+    "social.layout.description":
+      "Explore uma coleção das informações pessoais mais relevantes de Silas Rodrigues, projetos de código aberto e perfis de redes sociais. Conecte-se com ele por meio de plataformas como Portfolio, Github, Twitter e muito mais.",
 
-    'layout.scroll': 'Rolar para o topo',
+    "sr-only-language": "Alterar idioma",
+    "sr-only-theme": "Alternar tema",
+    "sr-only-menu": "Alternar menu",
+    "sr-only-page": "Página atual",
+    "sr-only-footer": "Rodapé",
+    "sr-only-header": "Cabeçalho",
 
-    'breadcrumb.home': 'Início',
-    'breadcrumb.social': 'Social',
-    'breadcrumb.about': 'Sobre',
-    'breadcrumb.contact': 'Contato',
-    'breadcrumb.privacy': 'Privacidade',
-    'breadcrumb.en': 'Inglês',
-    'breadcrumb.pt': 'Português',
+    "layout.scroll": "Rolar para o topo",
 
-    'privacy.layout.description':
-      'Nossa política de privacidade ajudará você a entender quais informações são coletadas e como elas são usadas.',
-    'privacy.layout.title': 'Bio - Silas Rodrigues | Política de Privacidade',
-    'privacy.title': 'Política de Privacidade',
-    'privacy.update': 'Última atualização: 15 de agosto de 2023',
-    'privacy.intro':
-      'Estamos comprometidos em proteger sua privacidade. Esta Política de Privacidade descreve como coletamos, usamos e protegemos suas informações pessoais quando você usa nosso site. Reserve um momento para ler este documento e entender nossas práticas.',
-    'privacy.item1.title': '1. Informações que coletamos',
-    'privacy.item1.subtitle':
-      'Podemos coletar os seguintes tipos de informações pessoais quando você interage com nosso site:',
-    'privacy.item1.content': 'Ao acessar a página de contato, coletamos apenas as informações que você fornece ao enviar o formulário. Não compartilhamos essas informações com terceiros.',
-    'privacy.item2.title': '2. Como usamos suas informações',
-    'privacy.item2.subtitle':
-      'Podemos usar suas informações pessoais para os seguintes fins:',
-    'privacy.item2.text': 'Comunicação: usamos apenas seu endereço de e-mail ou número de telefone para contato no contexto da mensagem recebida, sem nenhum tipo de spam.',
-    'privacy.contact.title': 'Entre em contato',
-    'privacy.contact.text': 'Se você tiver alguma dúvida ou preocupação com nossa Política de Privacidade, entre em contato conosco em.',
-    
+    "breadcrumb.home": "Início",
+    "breadcrumb.social": "Social",
+    "breadcrumb.about": "Sobre",
+    "breadcrumb.contact": "Contato",
+    "breadcrumb.privacy": "Privacidade",
+    "breadcrumb.resume": "Currículo",
+    "breadcrumb.en": "Inglês",
+    "breadcrumb.pt": "Português",
 
-    '404.title': 'Não encontrado | Bio - Silas Rodrigues',
-    '404.description':
-      'Página não encontrada. Por favor, verifique a URL na barra de endereços e tente novamente',
-    '404.status': 'Página não encontrada',
-    '404.message':
-      'Por favor, verifique a URL na barra de endereços e tente novamente.',
-    '404.button': 'Voltar para o início',
+    "privacy.layout.description":
+      "Nossa política de privacidade ajudará você a entender quais informações são coletadas e como elas são usadas.",
+    "privacy.layout.title": "Bio - Silas Rodrigues | Política de Privacidade",
+    "privacy.title": "Política de Privacidade",
+    "privacy.update": "Última atualização: 15 de agosto de 2023",
+    "privacy.intro":
+      "Estamos comprometidos em proteger sua privacidade. Esta Política de Privacidade descreve como coletamos, usamos e protegemos suas informações pessoais quando você usa nosso site. Reserve um momento para ler este documento e entender nossas práticas.",
+    "privacy.item1.title": "1. Informações que coletamos",
+    "privacy.item1.subtitle":
+      "Podemos coletar os seguintes tipos de informações pessoais quando você interage com nosso site:",
+    "privacy.item1.content":
+      "Ao acessar a página de contato, coletamos apenas as informações que você fornece ao enviar o formulário. Não compartilhamos essas informações com terceiros.",
+    "privacy.item2.title": "2. Como usamos suas informações",
+    "privacy.item2.subtitle":
+      "Podemos usar suas informações pessoais para os seguintes fins:",
+    "privacy.item2.text":
+      "Comunicação: usamos apenas seu endereço de e-mail ou número de telefone para contato no contexto da mensagem recebida, sem nenhum tipo de spam.",
+    "privacy.contact.title": "Entre em contato",
+    "privacy.contact.text":
+      "Se você tiver alguma dúvida ou preocupação com nossa Política de Privacidade, entre em contato conosco em.",
 
-    'header.about': 'Sobre',
-    'header.social': 'Social',
-    'header.contact': 'Contato',
+    "404.title": "Não encontrado | Bio - Silas Rodrigues",
+    "404.description":
+      "Página não encontrada. Por favor, verifique a URL na barra de endereços e tente novamente",
+    "404.status": "Página não encontrada",
+    "404.message":
+      "Por favor, verifique a URL na barra de endereços e tente novamente.",
+    "404.button": "Voltar para o início",
 
-    'footer.description':
-      'Bio - Silas Rodrigues. Carreira, projetos e maneiras de me encontrar online.',
-    'footer.nav': 'Navegação',
-    'footer.nav.home': 'Início',
-    'footer.nav.about': 'Sobre',
-    'footer.nav.social': 'Social',
-    'footer.nav.contact': 'Contato',
-    'footer.copy': 'Todos os direitos reservados.',
-    'footer.terms': 'Termos e Condições',
+    "header.about": "Sobre",
+    "header.social": "Social",
+    "header.contact": "Contato",
+
+    "footer.description":
+      "Bio - Silas Rodrigues. Carreira, projetos e maneiras de me encontrar online.",
+    "footer.nav": "Navegação",
+    "footer.nav.home": "Início",
+    "footer.nav.about": "Sobre",
+    "footer.nav.social": "Social",
+    "footer.nav.contact": "Contato",
+    "footer.copy": "Todos os direitos reservados.",
+    "footer.terms": "Termos e Condições",
   },
 } as const;
 
