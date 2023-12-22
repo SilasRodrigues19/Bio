01a1bcb736f7788a8689f65a3cae028710446c00
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-12-22 16:01:37 -0300

    docs: fix versioning problem with astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
+mport { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import linkPreview from 'astro-link-preview';
+import linkPreview from "astro-link-preview";
+/* import compress from "astro-compress"; */
 
-import compress from "astro-compress";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bio-silas.vercel.app/',
-  integrations: [tailwind(), sitemap(), linkPreview(), 
-    compress({
+  site: "https://bio-silas.vercel.app/",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    linkPreview(),
+/*     compress({
       SVG: false,
-      Exclude: [
-        (File) => File.match(/\/assets\/.*\.svg$/),
-      ]
-  })]
+      Exclude: [(File) => File.match(/\/assets\/.*\.svg$/)],
+    }), */
+  ],
+  output: "server",
+  adapter: vercel(),
 });
