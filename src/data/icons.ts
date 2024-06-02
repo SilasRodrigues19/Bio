81688f13ba3537ee058ce15268ae05161cd75857
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2024-06-01 23:49:54 -0300

    refactor: adjust skills section

---
+++ src/data/icons.ts
@@ -0,0 +1,34 @@
+export interface IconProps {
+  icon: string;
+  width?: number;
+  height?: number;
+}
+
+export const icons: IconProps[] = [
+  { icon: "flowbite:html-solid" },
+  { icon: "flowbite:css-solid" },
+  { icon: "simple-icons:alpinedotjs" },
+  { icon: "teenyicons:firebase-solid" },
+  { icon: "akar-icons:node-fill" },
+  { icon: "simple-icons:express" },
+  { icon: "simple-icons:fastify" },
+  { icon: "simple-icons:oracle" },
+  { icon: "file-icons:prisma" },
+  { icon: "file-icons:php" },
+  { icon: "mdi:laravel" },
+  { icon: "simple-icons:codeigniter" },
+  { icon: "mdi:docker" },
+  { icon: "bxl:postgresql" },
+  { icon: "devicon-plain:mysql" },
+  { icon: "file-icons:nextjs" },
+  { icon: "teenyicons:react-outline" },
+  { icon: "simple-icons:astro" },
+  { icon: "bxl:tailwind-css" },
+  { icon: "fa6-brands:sass" },
+  { icon: "simple-icons:chakraui" },
+  { icon: "mdi:material-ui" },
+  { icon: "file-icons:styledcomponents" },
+  { icon: "mdi:language-javascript" },
+  { icon: "mdi:language-typescript" },
+  { icon: "bxl:jquery" },
+];
